575f1c143e0a20e51546f8dab71476a515a2e263
ryeansharratt <ryean.ann@gmail.com>
2022-06-13 14:54:08 -0600

    Assignment #1
    Javascript course

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+window.alert("Hello World!");
+document.write("Hi World!");
+
+var A="This is a string.";
+document.write(A);
+
+var X="I am a variable, used for assignment #7."; //creating variables and writing them
+document.write(X);
+
+document.write("\"Hey Ryean, can you help me please?\" called dad. \"Yep I'll be right there!\" I called back.")
+document.write("\"Everyday I look outside," //how to use quotations and escape quotes (keeps JS from interpreting as end of the string)
++ "it reminds me that it\'s not so bad.\""
++"-someone");
+
+var C="\"Everyday I look outside," //concatenating
++ "it reminds me that it\'s not so bad.\""
++"-someone";
+document.write(C);
+
+var Family= "The Sharratt's", Dad ="Ethan", Mom="Erin", Daughter="Madyn";
+var Dad= Dad.fontcolor("blue"); //assigning colors to all of the Family variables
+var Mom= Mom.fontcolor("green");
+var Daughter= Daughter.fontcolor("pink");
+document.write(Mom);
+
+document.write(3+5) //writing an expression that would output 8
