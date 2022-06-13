baa84ced81d68ad14c80a926bbe85eccff3b0633
ryeansharratt <ryean.ann@gmail.com>
2022-06-13 15:49:50 -0600

    Assignment #2
    Project2_functions folder for assignment #2 in JS

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function() { //defining and naming a function
+    var str= "This text is blue.";//defining variable, giving it string value
+    var result= str.fontcolor("blue");//using fontcolor method on str variable
+    document.getElementById("Blue_Text").innerHTML=result//putting result into HTML text element with "Blue_text" id 
+}
+
+function myFunction() { //defining and naming a function
+    var sentence= "I love the "; //defining variable
+    sentence+= "outdoors so much!";//concatenating using the =+ operator
+    document.getElementById("Concatenate").innerHTML= sentence;//identifying JS to be called
+}
