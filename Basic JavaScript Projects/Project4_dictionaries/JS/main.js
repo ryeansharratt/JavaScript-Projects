3aaa4e7cd90e4f576b25bda3994c4516d658d331
ryeansharratt <ryean.ann@gmail.com>
2022-06-14 14:25:03 -0600

    Assignment #4
    JS assignment submission #4

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+function my_Dictionary() { //defining function of a dictionary
+    var Pet = { //declaring variable
+        Name:"Henry", //KVP's
+        Species:"Dog",
+        Color:"Brown, white, and black",
+        Breed:"Mutt",
+        Age:4,
+        Sound:"Woof!",
+    };
+    delete Pet.Name; //deletes this kvp
+    document.getElementById("Dictionary").innerHTML= Pet.Name; //results in undefined because kvp was deleted
+}
