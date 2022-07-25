708ebd21eecb62f095c14462c9ef1de3e36bc110
ryeansharratt <ryean.ann@gmail.com>
2022-07-25 14:34:05 -0600

    Includes Final assignment submission
    Portfolio assignment

---
+++ challenge#30.js
@@ -0,0 +1,26 @@
+function validateForm() {
+    let x = document.forms["myForm"]["number"].value;
+    if (x == "") {
+        alert("Must enter number");
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display="none";
+}
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
